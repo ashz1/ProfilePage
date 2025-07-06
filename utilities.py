7a24e3dfe77d731bae34e6d2812f55aa95b04c8a
ashz1 <aashayzende@gmail.com>
2025-07-06 19:26:13 -0400

    Create utilities.py

---
+++ utilities.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+def render_about_information():
+    st.markdown(
+        """
+        <h1 style='text-align: center;'>Aashay Zende</h1>
+        <h3 style='text-align: center;'>Business Analyst | Data Engineer | Educator</h3>
+        """, unsafe_allow_html=True
+    )
