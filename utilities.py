bf3e31a421abc1a16228b9046beaebfffcc5d538
ashz1 <aashayzende@gmail.com>
2025-07-06 22:50:59 -0400

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -5,5 +5,8 @@ def render_about_information():
         """
         <h1 style='text-align: center;'>Aashay Zende</h1>
         <h3 style='text-align: center;'>Business Analyst | Data Engineer | Educator</h3>
-        """, unsafe_allow_html=True
+        """,
+        unsafe_allow_html=True
     )
+
+    st.subheader("[aashayzende@gmail.com](mailto:aashayzende@gmail.com) • +1 857‑397‑2290 • Boston, MA", divider=True)
