053bc08822eacc1a9f198c22b1676b8c4f05ace7
ashz1 <aashayzende@gmail.com>
2025-07-07 15:22:01 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,46 +32,22 @@ tabs = st.tabs([
 # Assign tab components
 (
     tab_home,
-    tab_after_hours,
-    tab_carvana,
-    tab_ecommerce,
-    tab_powerbi,
+    
     tab_project_boston,
-    tab_random_widget,
-    tab_tableau
+    
 ) = tabs
 
 with tab_home:
     from sections.home import run as run_home
     run_home()
 
-with tab_after_hours:
-    from sections.after_hours import run as run_after_hours
-    run_after_hours()
 
-with tab_carvana:
-    from sections.carvana_case_study import run as run_carvana
-    run_carvana()
-
-with tab_ecommerce:
-    from sections.ecommerce_insights import run as run_ecommerce
-    run_ecommerce()
-
-with tab_powerbi:
-    from sections.powerbi import run as run_powerbi
-    run_powerbi()
 
 with tab_project_boston:
     from sections.project_boston import run as run_project_boston
     run_project_boston()
 
-with tab_random_widget:
-    from sections.random_widget_generator import run as run_random_widget
-    run_random_widget()
 
-with tab_tableau:
-    from sections.tableau_dashboard import run as run_tableau
-    run_tableau()
 
 st.divider()
 st.markdown('[Back to Top](#aashay‑zende‑portfolio)')
