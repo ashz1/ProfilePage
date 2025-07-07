3c146b397fda4fd836039aa2461fd2290bbfd832
ashz1 <aashayzende@gmail.com>
2025-07-06 22:54:27 -0400

    Update utilities.py

--- utilities.py
+++ utilities.py
@@ -4,8 +4,9 @@ def render_about_information():
     st.markdown(
         """
         <h1 style='text-align: center;'>Aashay Zende</h1>
-        <h3 style='text-align: center;'>[aashayzende@gmail.com](mailto:aashayzende@gmail.com) • +1 857‑397‑2290 • Boston, MA</h1>
         <h3 style='text-align: center;'>Business Analyst | Data Engineer | Educator</h3>
+        <h6 style='text-align: center;'>[aashayzende@gmail.com](mailto:aashayzende@gmail.com) • +1 857‑397‑2290 • Boston, MA</h6>
+
         """,
         unsafe_allow_html=True
     )
