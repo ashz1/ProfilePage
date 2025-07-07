07ddbedbd3106a2bc79643555f07253d93e3e6f8
ashz1 <aashayzende@gmail.com>
2025-07-06 20:10:36 -0400

    Update home.py

--- sections/home.py
+++ sections/home.py
@@ -6,10 +6,10 @@ def run():
     st.write("""
     Aashay Zende • aashayzende@gmail.com • +1 857‑397‑2290 • Boston, MA
 
-I turn data into decisions. As a Business Analyst with a master’s in Business Analytics and a background in engineering, I’ve built end‑to‑end BI solutions that bridge strategy and technology, guiding global teams to clearer insights and smarter actions. Whether crafting interactive dashboards, designing automated data pipelines, or developing forecasting models with high accuracy, I focus on reliability and impact. My collaborative approach brings together finance, product, and development stakeholders to co‑create solutions that streamline processes and spotlight growth opportunities.  /n
+I turn data into decisions. As a Business Analyst with a master’s in Business Analytics and a background in engineering, I’ve built end‑to‑end BI solutions that bridge strategy and technology, guiding global teams to clearer insights and smarter actions. Whether crafting interactive dashboards, designing automated data pipelines, or developing forecasting models with high accuracy, I focus on reliability and impact. My collaborative approach brings together finance, product, and development stakeholders to co‑create solutions that streamline processes and spotlight growth opportunities.  \n
 
-Beyond consulting, I’m driven to lift up underserved youth through hands‑on teaching in programming, 3D design, and robotics.  /n
+Beyond consulting, I’m driven to lift up underserved youth through hands‑on teaching in programming, 3D design, and robotics.  \n
      
-Off the clock, I’m usually behind a camera in nature, catching waves, kicking a soccer ball, or working on a canvas—see the hobbies section for more.  /n
+Off the clock, I’m usually behind a camera in nature, catching waves, kicking a soccer ball, or working on a canvas—see the hobbies section for more.  \n
               """)
 st.button("Contact Me", key="home_contact") 
