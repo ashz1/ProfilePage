f45c41b67bd30f2dd905ae8e085656604799f657
ashz1 <aashayzende@gmail.com>
2025-07-07 22:09:34 -0400

    Update home.py

--- sections/home.py
+++ sections/home.py
@@ -39,13 +39,13 @@ def run():
     timeline_data = {
         "title": {"media": {"url": ""}, "text": {"headline": "Career Timeline", "text": "Professional and Academic Highlights"}},
         "events": [
-            {"start_date": {"year": "2018", "month": "08"}, "text": {"headline": "BTech - Automobile Engineering, MIT Manipal"}},
-            {"start_date": {"year": "2019", "month": "03"}, "text": {"headline": "Research Project - LoopMIT, Dept Head"}},
+            {"start_date": {"year": "2018", "month": "08"}, "end_date": {"year": "2022", "month": "06"}, "text": {"headline": "BTech - Automobile Engineering, MIT Manipal"}},
+            {"start_date": {"year": "2019", "month": "03"}, "end_date": {"year": "2021", "month": "05"}, "text": {"headline": "Research Project - LoopMIT, Dept Head"}},
             {"start_date": {"year": "2021", "month": "05"}, "end_date": {"year": "2021", "month": "07"}, "text": {"headline": "Mahindra & Mahindra, R&D Intern"}},
             {"start_date": {"year": "2022", "month": "02"}, "end_date": {"year": "2023", "month": "05"}, "text": {"headline": "Business Analyst - Redseer Strategy"}},
-            {"start_date": {"year": "2023", "month": "09"}, "text": {"headline": "MS in Business Analytics - Northeastern"}},
+            {"start_date": {"year": "2023", "month": "09"}, "end_date": {"year": "2024", "month": "12"}, "text": {"headline": "MS in Business Analytics - Northeastern"}},
             {"start_date": {"year": "2024", "month": "01"}, "text": {"headline": "Business Analyst - Radiant Consulting"}},
-            {"start_date": {"year": "2024", "month": "04"}, "text": {"headline": "Mentor & Teacher - TSNE Pathway / Dudley Charter"}},
+            {"start_date": {"year": "2024", "month": "04"}, "text": {"headline": "Mentor & Teacher - TSNE Pathway / Dudley St Charter School"}},
             {"start_date": {"year": "2024", "month": "06"}, "end_date": {"year": "2024", "month": "12"}, "text": {"headline": "Business Analyst Intern - Clatch Fund"}},
         ]
     }
