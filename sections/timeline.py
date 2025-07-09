ecb1feaa3c85980d507de61095ad944613cc05ac
ashz1 <aashayzende@gmail.com>
2025-07-09 10:01:37 -0400

    Update timeline.py

--- sections/timeline.py
+++ sections/timeline.py
@@ -15,44 +15,44 @@ def run():
             }
         },
         "events": [
-            {
-                "start_date": {"year": "2018", "month": "08"},
-                "end_date": {"year": "2022", "month": "06"},
-                "text": {"headline": "BTech - Automobile Engineering, MIT Manipal"}
-            },
-            {
-                "start_date": {"year": "2019", "month": "03"},
-                "end_date": {"year": "2021", "month": "05"},
-                "text": {"headline": "Research Project - LoopMIT, Dept Head"}
-            },
-            {
-                "start_date": {"year": "2021", "month": "05"},
-                "end_date": {"year": "2021", "month": "07"},
-                "text": {"headline": "Mahindra & Mahindra, R&D Intern"}
-            },
-            {
-                "start_date": {"year": "2022", "month": "02"},
-                "end_date": {"year": "2023", "month": "05"},
-                "text": {"headline": "Business Analyst - Redseer Strategy"}
-            },
-            {
-                "start_date": {"year": "2023", "month": "09"},
-                "end_date": {"year": "2024", "month": "12"},
-                "text": {"headline": "MS in Business Analytics - Northeastern"}
-            },
-            {
-                "start_date": {"year": "2024", "month": "01"},
-                "text": {"headline": "Business Analyst - Radiant Consulting"}
-            },
-            {
-                "start_date": {"year": "2024", "month": "04"},
-                "text": {"headline": "Mentor & Teacher - TSNE Pathway / Dudley St Charter School"}
-            },
-            {
-                "start_date": {"year": "2024", "month": "06"},
-                "end_date": {"year": "2024", "month": "12"},
-                "text": {"headline": "Business Analyst Intern - Clatch Fund"}
-            },
+        {
+            "start_date": {"year": "2018", "month": "08"},
+            "end_date": {"year": "2022", "month": "07"},
+            "text": {"headline": "BTech - Automobile Engineering, MIT Manipal"}
+        },
+        {
+            "start_date": {"year": "2019", "month": "03"},
+            "end_date": {"year": "2021", "month": "05"},
+            "text": {"headline": "Research Project - LoopMIT, Dept Head"}
+        },
+        {
+            "start_date": {"year": "2021", "month": "05"},
+            "end_date": {"year": "2021", "month": "07"},
+            "text": {"headline": "R&D Intern - Mahindra & Mahindra"}
+        },
+        {
+            "start_date": {"year": "2022", "month": "02"},
+            "end_date": {"year": "2023", "month": "05"},
+            "text": {"headline": "Business Analyst - Redseer Strategy"}
+        },
+        {
+            "start_date": {"year": "2023", "month": "09"},
+            "end_date": {"year": "2024", "month": "12"},
+            "text": {"headline": "MS in Business Analytics - Northeastern University"}
+        },
+        {
+            "start_date": {"year": "2024", "month": "01"},
+            "text": {"headline": "Business Analyst - Radiant Consulting"}
+        },
+        {
+            "start_date": {"year": "2024", "month": "04"},
+            "text": {"headline": "Mentor & Teacher - TSNE / Dudley St Charter School"}
+        },
+        {
+            "start_date": {"year": "2024", "month": "06"},
+            "end_date": {"year": "2024", "month": "12"},
+            "text": {"headline": "Business Analyst Intern - Clatch Fund"}
+        }
         ]
     }
 
