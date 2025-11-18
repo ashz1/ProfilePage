fcb47de8a080d60c444309156a0b6d77636ed865
ashz1 <aashayzende@gmail.com>
2025-11-17 19:16:51 -0500

    Update home.py

--- sections/home.py
+++ sections/home.py
@@ -12,26 +12,13 @@ def run():
 
         My collaborative approach brings together finance, product, and development stakeholders to co‑create solutions that streamline processes and spotlight growth opportunities.
 
-        Beyond consulting, I’m driven to lift up underserved youth through hands‑on teaching and mentoring. I teach game programming, 3D design & printing, and robotics at Dudley Street Charter School and Pathway Initiative – TSNE Boston.
-
-        Off the clock, I’m usually behind a camera in nature, catching waves, kicking a soccer ball, or working on a canvas — see the hobbies section for more.
+        Beyond consulting, I’m driven to lift up underserved youth through hands‑on teaching and mentoring. I teach game programming, 3D design & printing, and robotics. Off the clock, I’m usually behind a camera in nature, catching waves, kicking a soccer ball, or working on a canvas — see the hobbies section for more.
         """)
 
     with col2:
         st.image("6.jpg", use_container_width=True)
 
-    # Display Resume PDF
-    pdf_file_path = "Aashay R Zende - Resume.pdf"
-    pdf_viewer(pdf_file_path, width=700, height=1000)
 
-    # Download Button
-    with open(pdf_file_path, "rb") as f:
-        st.download_button(
-            label="Download Resume",
-            data=f,
-            file_name="Aashay_R_Zende_Resume.pdf",
-            mime="application/pdf",
-            use_container_width=True
         )
 
 
